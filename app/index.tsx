43b04c524a83231ca6689f170496f060e4ed13e3
phplemos <phplemos.dev@gmail.com>
2024-10-20 19:30:52 -0300

    Primeiro commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+export default function Home() {
+  return (
+    <>
+      <View className="flex-1 justify-center items-center">
+        <Text>Hello World</Text>
+      </View>
+    </>
+  );
+}
